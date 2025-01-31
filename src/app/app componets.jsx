75f0071216069a3f3ceb130326a183dd43a6efe3
Ebunoluwa Giobari Akinwumi <Ebuoluwaakinwumi@gmail.com>
2025-01-31 21:53:10 +0100

    ..

---
+++ src/app/app componets.jsx
@@ -0,0 +1,20 @@
+"use client"
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './navbar/navbar';
+import Footer from './footer-components/footer';
+
+function Appsection() {
+    return (
+      <div className='app h-100%'>
+          <div  className='sticky top-0 z-50'><Navbar/></div>
+
+          <main>
+            <Outlet/>
+          </main>
+          <Footer/>
+      </div>
+    );
+  }
+  
+  export default Appsection;
